446ff563f7e844a24c35fac5f1bdf881d4a20243
Kevin McNamara <kevinmcnamara@diatom.local>
2019-03-16 22:18:45 -0400

    First Push

--- stockreader.py
+++ stockreader.py
@@ -3,30 +3,23 @@
 Created on Tue May 16 18:48:53 2017
 
 @author: Kevin McNamara
+@version 2.00.mar.9.2019
 """
 import requests
 import re
 import csv
 import sys
 import datetime
+import optparse
+
+#option parser
+parser = optparse.OptionParser()
+parser.add_option('-q', '--query', action="store", dest="query", help="query string", default="nothing")
+options, args = parser.parse_args()
 
 #Global Constants
-linkNASADAQ = 'http://eoddata.com/stocklist/NASDAQ/'
-linkNYSE = 'http://eoddata.com/stocklist/NYSE/'
-alphalist = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-filename = 'stocks.csv'
-
-#Fomat Data before writing to file
-def removeBlankLine(data1):
-    reader = csv.reader(data1.split('\n'), delimiter='|')
-    utc_datetime = datetime.datetime.utcnow()
-    d1=utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    for row in reader:
-        if len(row)!=0:
-            #DayOfWeek,GMTLong,DateEST,Symbol,Description,High,Low,Close,Volume,Change
-            rowout=d1+',\t'.join(row)+','+'\n'
-            print(rowout) 
-    return()
+linkMarket = 'https://www.marketwatch.com/investing/stock/' + options.query
+filename = 'stocks1.csv'
 
 #this function writes rows to a file
 def writeToFile(data):
@@ -41,30 +34,41 @@ def writeToFile(data):
 #this function formats a raw string to a line return row string      
 def fomatdata(txt):
         d = ('\n'.join([x for x in txt.split("\n") if x.strip()!=''])+'\n')
-        removeBlankLine(d)       
+        #removeBlankLine(d)       
         return()
 
-#this function reads a web form and strips out the raw data
-def parsedata(urlin):
-    f = requests.get(urlin)    
-    data0 = re.sub('<[^>]*>', '|', f.text)
-    data1 = data0.replace(',','')
-    data = data1.replace('|',',')
-    do1= data.replace(',,,,', ',').replace(',,,', ',').replace(',,', ',').replace(',&nbsp;,', '').strip().strip(',') 
-    begin_index = do1.index("W,X,Y,Z,")
-    end_index = do1.index(".ld")
-    dataout =  do1[begin_index+50:end_index-37].strip().strip(',').replace('Change','')
-    fomatdata(dataout)
-    return(dataout)
+#this is a dataparser
+def parsedata1(urlin):
+    #read from url
+    f = requests.get(urlin)
+    big = f.text
+    #parse specific section
+    sstart = big.index('<title>')+7
+    eend = big.index('keywords')-12	
+    d = f.text[sstart:eend].strip()
+    # 1rst filter
+    filter1 = d.find('meta property')
+    d0 = d[filter1:len(d)]
+    # 2nd filter
+    filter2 = d0.find('<meta name=')
+    d1 = d0[filter2:len(d0)].strip().replace(',','').replace('<meta name','').replace('>',',').replace('content',':').replace('=','').replace('<meta property"og:title','"long name').replace(' ','')
+    # 3rd filter
+    filter3 = d1.find('"tickerSymbol')
+    d2 = d1[filter3:len(d1)]
+    # remove blanks and returned lines
+    d3 = d2.replace("\n","")
+    # format to json
+    d4 = '{"datapoint":{"' + d3[1:len(d3)-1] + '}}'
+    print d4
+    return(d4)
 
-#this function calls a webform and iterates through an array of pages
+#this function calls a webform
 def buildurl(webcall):
-    for letter in alphalist:
-        webcallA=webcall+letter+'.htm'
-        parsedata(webcallA)
-    return()
+        parsedata1(webcall)
+	return()
 
-buildurl(linkNASADAQ)
-buildurl(linkNYSE)
-
+try:
+   buildurl(linkMarket)
+except:
+   print ('An exception occured when connecting to the Market link!')
 exit
